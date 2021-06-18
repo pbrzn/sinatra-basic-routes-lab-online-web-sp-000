770654d8bcd1d7b92263a8586634bc7a00e76d19
Patrick Brennan <pbrennanmusic@gmail.com>
2021-06-17 23:39:18 -0400

    commit tree eed928e6ccf9cbd9c1ea54fc4ff7b1cf9bc698d8

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Patrick."
+  end
+
+  get '/hometown' do
+    "My hometown is Long Island."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is \"For No One\" by The Beatles."
+  end
 end
